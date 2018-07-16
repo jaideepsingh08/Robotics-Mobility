2b0759d5311e2c278098d335d9ea243e2d4f2e5c
jaideepsingh08 <37825086+jaideepsingh08@users.noreply.github.com>
2018-07-16 02:25:27 -0400

    Initial commit

---
+++ Quiz3.1.1_v2/Q1/computeForwardKinematics.m
@@ -0,0 +1,10 @@
+function endeff = computeForwardKinematics(rads)
+
+
+
+x = 1*cos(rads);
+y = sin(rads);
+
+
+endeff = [x,y];
+
---
+++ Quiz3.1.1_v2/Q3/computeRrInverseKinematics.m
@@ -0,0 +1,9 @@
+function [rads1,rads2] = computeRrInverseKinematics(X,Y)
+
+syms theta1 theta2 ;
+eqn1 = cos(theta1+theta2)+cos(theta1) == X;
+eqn2 = sin(theta1+theta2)+sin(theta1) == Y;
+eqns = [eqn1,eqn2];
+S = solve(eqns)
+rads1 = double(S.theta1(1,1))
+rads2 = double(S.theta2(1,1))
